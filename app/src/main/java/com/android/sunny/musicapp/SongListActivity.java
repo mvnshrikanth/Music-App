ba4f14fc5261c11833a68a5b6e9cc9fe4f1ce836
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2016-10-09 23:15:46 -0400

    Layout design continuation ...

--- app/src/main/java/com/android/sunny/musicapp/SongListActivity.java
+++ app/src/main/java/com/android/sunny/musicapp/SongListActivity.java
@@ -1,32 +1,37 @@
 package com.android.sunny.musicapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SongListActivity extends AppCompatActivity {
+
+    final private String LOG_TAG = SongListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_song_list);
 
-        ListView listView = (ListView) findViewById(R.id.folder_list_view);
-        String[] musicStrings = getResources().getStringArray(R.array.folderlist);
+        ListView listView = (ListView) findViewById(R.id.music_list_view);
+        final String[] musicStrings = getResources().getStringArray(R.array.musiclist);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, musicStrings);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, SongListActivity.class);
-                startActivity(intent);
+                TextView textView = (TextView) findViewById(R.id.song_name_text_view);
+                textView.setText(musicStrings[position]);
             }
         });
+    }
+
+    public void nowPlaying(View view) {
 
     }
 }
